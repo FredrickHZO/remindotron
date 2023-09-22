f0630a1e90f16658dec4c435d35ec0e533a9bf9d
FredrickHZO <federico.armagno97@gmail.com>
2023-09-22 22:43:09 +0200

    Add 'Put' and 'Get'.

--- cache.go
+++ cache.go
@@ -1,1 +1,35 @@
 package main
+
+import (
+	"github.com/akrylysov/pogreb"
+)
+
+type Cache string
+
+func (c Cache) Put(key, val []byte) error {
+	db, err := pogreb.Open(string(c), nil)
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	err = db.Put(key, val)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c Cache) Get(key []byte) ([]byte, error) {
+	db, err := pogreb.Open(string(c), nil)
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close()
+
+	val, err := db.Get(key)
+	if err != nil {
+		return nil, err
+	}
+	return val, nil
+}
